54766ad93487808b8be7dcfcf3863ffc286e93da
Fotis Balampanis <fbalampanis@protonmail.com>
2024-04-08 16:24:54 +0300

    Init raw commit

--- cdsetool-cli/cdsetool_cli/cli.py
+++ cdsetool-cli/cdsetool_cli/cli.py
@@ -5,10 +5,21 @@ import click
 # Appending the module path in order to have a kind of cli "dry execution"
 sys.path.append(str(Path(__file__).parent / ".."))
 
-from cdsetool_cli.utils import download_data, available_parameters # noqa:402
+
+from cdsetool_cli.utils import (  # noqa:402
+    download_data_copernicus,
+    available_parameters,
+    query_items,
+    query_data_modis
+ )
 
 
-@click.command(
+@click.group()
+def cli():
+    pass
+
+
+@cli.command(
     help=(
         "Downloads Copernicus data according to parameters as defined in the [CONFIG_FILE],"
         " or shows available query parameters for Satellites present in the [CONFIG_FILE]"
@@ -32,16 +43,37 @@ from cdsetool_cli.utils import download_data, available_parameters # noqa:402
     is_flag=True,
     help="Show available query Parameters for Satellites present in CONFIG_FILE",
 )
+@click.option(
+    "--query",
+    "-q",
+    is_flag=True,
+    help="Show multitude of available items for query in CONFIG_FILE",
+)
 @click.argument("config_file", required=True)
-def cli(download, parameters, config_file, verbose):
+def copernicus(download, parameters, query, config_file, verbose):
     if parameters:
         click.echo("Available parameters per data source list:\n")
         available_parameters(config_file)
     elif download:
         click.echo("Downloading:\n")
-        download_data(config_file, verbose)
+        download_data_copernicus(config_file, verbose)
+    elif query:
+        click.echo("Querying:\n")
+        query_items(config_file)
     else:
         click.echo("Please select at least one option (-d, -p)")
+
+
+@cli.command()
+@click.option(
+    "--query",
+    "-q",
+    is_flag=True,
+    help="Show multitude of available items for query in CONFIG_FILE",
+)
+@click.argument("config_file", required=True)
+def modis(query, config_file):
+    query_data_modis(config_file)
 
 
 if __name__ == "__main__":  # pragma: no cover
--- cdsetool-cli/cdsetool_cli/utils.py
+++ cdsetool-cli/cdsetool_cli/utils.py
@@ -1,5 +1,8 @@
 import os
+import sys
 import json
+# import netrc
+
 import click
 import pathlib
 
@@ -8,6 +11,8 @@ from cdsetool.query import describe_collection
 from cdsetool.download import download_features
 from cdsetool.credentials import Credentials
 from cdsetool.monitor import StatusMonitor
+
+import earthaccess
 
 
 def available_parameters(config_file) -> None:
@@ -23,9 +28,63 @@ def available_parameters(config_file) -> None:
         click.echo(f"{satellite} available search terms: \n {search_terms}\n")
 
 
+def query_data_copernicus(config_file):
+
+    with open(f"config/{config_file}") as f:
+        config = json.load(f)
+
+    for satellite, search_terms in config.items():
+        features = list(query_features(satellite, search_terms))
+        click.echo(f"Available items for {satellite}: {len(features)} \n")
+
+
+def query_items(config_file):
+
+    with open(f"config/{config_file}") as f:
+        config = json.load(f)
+
+    for satellite, search_terms in config.items():
+        features = list(query_features(satellite, search_terms))
+        click.echo(f"Available items for {satellite}: {len(features)} \n")
+
+
+# TODO introduce checking of netrc for borh copernicus and earth data
+# netrc.netrc().authenticators("urs.earthdata.nasa.gov")
+def query_data_modis(config_file):
+    earthaccess.login()
+    download_path = pathlib.Path("./data").absolute()
+    download_path.mkdir(parents=True, exist_ok=True)
+
+    search_terms = ""
+    config = None
+    download_path = pathlib.Path("./data").absolute()
+    download_path.mkdir(parents=True, exist_ok=True)
+
+    with open(f"config/{config_file}") as f:
+        config = json.load(f)
+
+    for collection, terms in config.items():
+        search_terms = terms
+
+        bbox = tuple(float(i) for i in search_terms["box"].split(","))
+        print(bbox)
+        start_date = search_terms["startDate"]
+        end_date = search_terms["completionDate"]
+
+        results = earthaccess.search_data(
+            short_name=terms["short_name"],
+            cloud_hosted=True,
+            bounding_box=bbox,
+            temporal=(start_date, end_date)
+        )
+        print(len(results))
+        # files = earthaccess.download(results, download_path)
+    # print(len(files))
+
+
 # TODO Logging
 # TODO typing
-def download_data(config_file, verbose):
+def download_data_copernicus(config_file, verbose):
     """
         Downloads using the query terms of config_file. Verbose is used for detailed
         progress bar indicators. Download is using a concurrency setting of 4 threads.
@@ -49,6 +108,8 @@ def download_data(config_file, verbose):
         features = list(query_features(satellite, search_terms))
         click.echo(f"Available items for {satellite}: {len(features)} \n")
 
+        sys.stdout.flush()
+
         list(
             download_features(
                 features,
@@ -56,3 +117,36 @@ def download_data(config_file, verbose):
                 {"concurrency": 4, "monitor": monitor, "credentials": credentials},
             )
         )
+
+
+# TODO introduce checking of netrc for borh copernicus and earth data
+# netrc.netrc().authenticators("urs.earthdata.nasa.gov")
+def download_data_modis(config_file):
+    earthaccess.login()
+    download_path = pathlib.Path("./data").absolute()
+    download_path.mkdir(parents=True, exist_ok=True)
+
+    search_terms = ""
+    config = None
+    download_path = pathlib.Path("./data").absolute()
+    download_path.mkdir(parents=True, exist_ok=True)
+
+    with open(f"config/{config_file}") as f:
+        config = json.load(f)
+
+    for collection, terms in config.items():
+        search_terms = terms
+
+        bbox = tuple(float(i) for i in search_terms["box"].split(","))
+        start_date = search_terms["startDate"]
+        end_date = search_terms["completionDate"]
+
+        results = earthaccess.search_data(
+            short_name=terms["short_name"],
+            cloud_hosted=True,
+            bounding_box=bbox,
+            temporal=(start_date, end_date)
+        )
+        print(len(results))
+        files = earthaccess.download(results, download_path)
+        print(len(files))
