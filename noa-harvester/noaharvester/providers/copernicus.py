0565f3f483dba8e8138003485c1560e695f2ccdd
Fotis Balampanis <fbalampanis@protonmail.com>
2024-04-24 15:12:30 +0300

    Copernicus type hints documentation comments

--- noa-harvester/noaharvester/providers/copernicus.py
+++ noa-harvester/noaharvester/providers/copernicus.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import sys
 import click
 
@@ -10,7 +12,26 @@ from noaharvester.providers import DataProvider
 
 
 class Copernicus(DataProvider):
-    def __init__(self, verbose=False):
+    """
+    The Copernicus service data provider, implementing query, download and describe collection
+    functions. It uses (wraps) the cdsetool Python package.
+
+    Properties:
+        credentials: returns the Credentials (cdsetool) instance
+    """
+    def __init__(self, verbose: bool = False) -> Copernicus:
+        """
+        Copernicus data provider.
+
+        Parameters:
+            verbose (bool): If True, download progress indicator will be visible
+
+        Methods:
+        query (item): Query a collection based on search terms in [item].
+                      Item also includes the collection name.
+        download (item): Download a collection [item].
+        describe (collection): Output available search terms of [collection].
+        """
         super().__init__()
 
         # From .netrc (or _netrc for Windows)
@@ -19,26 +40,50 @@ class Copernicus(DataProvider):
         self._monitor = StatusMonitor() if verbose else False
 
     @property
-    def credentials(self):
+    def credentials(self) -> Credentials:
+        """Return the credentials instance."""
         return self._credentials
 
-    def query(self, item):
+    def query(self, item: dict) -> tuple[str, int]:
+        """
+        Query Copernicus for item["collection"], item["search_terms"]] items.
 
+        Parameters:
+            item (dict): Dictionary as per config file structure.
+
+        Returns:
+            tuple (str, int):  Collection name, sum of available items.
+        """
         features = list(query_features(item["collection"], item["search_terms"]))
         click.echo(f"Available items for {item['collection']}: {len(features)} \n")
         return item["collection"], len(features)
 
-    def describe(self, collection):
+    def describe(self, collection: str) -> tuple[str, list]:
+        """
+        Ask Copernicus for describe the collection.
 
+        Parameters:
+            collection (string): The collection to be queried.
+
+        Returns:
+            tuple (string, list):  Collection name, list of available search terms.
+        """
         search_terms = describe_collection(collection).keys()
         click.echo(f"{collection} available search terms: \n {search_terms}\n")
 
         return collection, list(search_terms)
 
-    def download(self, item):
+    def download(self, item: dict) -> tuple[str, int]:
         """
-        Downloads using the query terms of config_file. Verbose is used for detailed
-        progress bar indicators. Download is using a concurrency setting of 4 threads.
+        Download from Copernicus from item["collection"] the item["search_terms"]] items.
+        Download is using a concurrency setting of 4 threads and stored in local execution
+        folder, under /data.
+
+        Parameters:
+            item (dict): Dictionary as per config file structure.
+
+        Returns:
+            tuple (string, int):  Collection name, sum of downloaded files.
         """
 
         self._download_path.mkdir(parents=True, exist_ok=True)
