2620dcaf2ff1f673403ce05591e6e81612006587
Fotis Balampanis <fbalampanis@protonmail.com>
2024-09-19 15:44:05 +0300

    First simple cli version of S2 to STAC json

---
+++ noa-stac-ingest/noastacingest/cli.py
@@ -0,0 +1,61 @@
+"""Cli for NOA-Beyond STAC Ingest processor.
+
+This interface and processor are used to create STAC Items
+from various data sources.
+"""
+
+from __future__ import annotations
+import sys
+import logging
+from pathlib import Path
+
+import click
+from click import Argument  # , Option
+
+# Appending the module path in order to have a kind of cli "dry execution"
+sys.path.append(str(Path(__file__).parent / ".."))
+
+from noastacingest import ingest  # noqa:402 pylint:disable=wrong-import-position
+
+logger = logging.getLogger(__name__)
+
+
+@click.group(
+    help=(
+        "Creates STAC Items from filename, "
+        "according to parameters as defined in the [CONFIG_FILE]."
+    )
+)
+@click.option(
+    "--log",
+    default="warning",
+    help="Log level (optional, e.g. DEBUG. Default is WARNING)",
+)
+def cli(log):
+    """Click cli group for query, download, describe cli commands"""
+    numeric_level = getattr(logging, log.upper(), "WARNING")
+    logging.basicConfig(level=numeric_level, format="%(asctime)s %(message)s")
+
+
+@cli.command(help="Create STAC Item from SAFE filename")
+@click.argument("input", required=True)
+@click.argument("config", required=False)
+def create_item(input_path: Argument | str, config: Argument | str) -> None:
+    """
+    Instantiate Ingest Class and call "create_item"
+
+    Parameters:
+        input (click.Argument | str): Input filename path
+        config_file (click.Argument | str) - optional: config json file
+            selecting file types etc
+    """
+    if config:
+        logger.debug("Cli STAC creation using config file: %s", config)
+
+    ingestor = ingest.Ingest(config=config)
+    click.echo("Ingesting single item from file\n")
+    ingestor.single_item(input_path)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    cli()
--- noa-stac-ingest/noastacingest/ingest.py
+++ noa-stac-ingest/noastacingest/ingest.py
@@ -1,4 +1,34 @@
+"""Main Ingest module."""
+from __future__ import annotations
+from pathlib import Path
+
 from stactools.sentinel2.commands import create_item
 
 
-item = create_item()
+class Ingest:
+    """
+    A class
+    """
+
+    def __init__(
+        self,
+        config: str | None
+    ) -> Ingest:
+        """
+        Ingest main class implementing single and batch item creation.
+        """
+        if config:
+            self._config = config
+
+    def single_item(self, path):
+        """
+        Just an item
+        """
+
+        item = create_item(path)
+
+        # Define the path to save the JSON file
+        json_file_path = str(Path(Path(path).parent, "output.json"))
+
+        # Save the item as a JSON file
+        item.save_object(dest_href=json_file_path)
