48bd180a4337ccc0d490b1ef1cff81460dfaedf0
Fotis Balampanis <fbalampanis@protonmail.com>
2024-10-29 16:23:02 +0200

    Rest of db utils

--- noa-harvester/noaharvester/db/utils.py
+++ noa-harvester/noaharvester/db/utils.py
@@ -1,26 +1,85 @@
+from pathlib import Path
 from configparser import ConfigParser
 import psycopg
 
-def load_config(filename='database.ini', section='postgresql-sentinel-status'):
+def load_config(filename="database.ini", section="sentinel_status"):
     # TODO: has default section? Does check or not?
     parser = ConfigParser()
-    parser.read(filename)
+    config_path = str(Path(Path(__file__).parent, filename).resolve())
+    parser.read(config_path)
 
-    # get section, default to postgresql
     config = {}
     if parser.has_section(section):
         params = parser.items(section)
         for param in params:
-            config[param[0]] = param[1]
+            config[param[0]] = str(param[1])
     else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+        raise Exception("Section {0} not found in the {1} file".format(section, filename))
 
     return config
 
 
-def connect(config):
-    """ Connect to the PostgreSQL database server """
-    with psycopg.connect(config) as conn:
-        print('Connected to the PostgreSQL server.')
+def describe_table(config, table):
+
+    sql = f"""
+        SELECT column_name
+        FROM information_schema.columns
+        WHERE table_name = %s
+        ORDER BY ordinal_position;
+        """
+
+    with psycopg.connect(**config) as conn:
         with conn.cursor() as curs:
-            curs.execute("SQL1")
+            curs.execute(sql, (table,))
+            columns = [row[0] for row in curs.fetchall()]
+            print("Column names:", columns)
+
+
+def query_uuid(config, uuid):
+
+    sql = f"""
+        SELECT *
+        FROM products
+        where products.id=%s"""
+
+    with psycopg.connect(**config) as conn:
+        with conn.cursor() as curs:
+            curs.execute(sql, (uuid,))
+            # It should be one...
+            return curs.fetchone()
+
+def update_uuid(config, uuid, column, value):
+        
+    sql = f"""
+        UPDATE products
+        SET {column} = %s
+        WHERE products.id = %s;
+    """
+    if column == "id":
+        # Do not explain
+        return False
+
+    with psycopg.connect(**config) as conn:
+        with conn.cursor() as curs:
+            curs.execute(sql, (value, uuid))
+            conn.commit()
+
+            # If updated succesfully, return True
+            if curs.rowcount > 0:
+                return True
+            return False
+
+def query_all_items(config):
+    sql = """
+        SELECT *
+        FROM products
+    """
+
+    with psycopg.connect(**config) as conn:
+        print("Connected to the PostgreSQL server.")
+        with conn.cursor() as curs:
+            print("Querying")
+            curs.execute(sql)
+            results = curs.fetchall()
+            for row in results:
+                print(row)
