fb40733f72a28d541da3b4f5521f39e662e64f6f
Fotis Balampanis <fbalampanis@protonmail.com>
2024-10-31 16:48:54 +0200

    Refactor utils a bit

--- noa-harvester/noaharvester/db/utils.py
+++ noa-harvester/noaharvester/db/utils.py
@@ -1,9 +1,19 @@
+"""DB utility functions for postgre interfacing"""
+
 import os
 from pathlib import Path
 from configparser import ConfigParser
 import psycopg
+# pylint: disable=E1129 # False-positive for psycopg connection context manager
+# TODO: after integration tests, remove helper functions and make
+# table specific functions generic (by posting table name also)
+
 
 def get_local_config(filename="database.ini", section="sentinel_status"):
+    """
+    Gets local config, found by default in database.ini
+     of the same path as utils
+    """
     # TODO: has default section? Does check or not?
     parser = ConfigParser()
     config_path = str(Path(Path(__file__).parent, filename).resolve())
@@ -15,11 +25,13 @@ def get_local_config(filename="database.ini", section="sentinel_status"):
         for param in params:
             config[param[0]] = str(param[1])
     else:
-        raise Exception("Section {0} not found in the {1} file".format(section, filename))
+        raise RuntimeError(f"Section {section} not found in the {filename} file")
 
     return config
 
+
 def get_env_config():
+    """Get db configuration from env variables: proper way in Kubernetes ecosystem"""
     # TODO make checks
     # TODO check if other db access is needed: (like "section" in local config)
     config = {}
@@ -32,9 +44,10 @@ def get_env_config():
         return None
     return config
 
-def describe_table(config, table):
 
-    sql = f"""
+def describe_table(config, table):
+    """Helper to get all columns of a table"""
+    sql = """
         SELECT column_name
         FROM information_schema.columns
         WHERE table_name = %s
@@ -49,11 +62,12 @@ def describe_table(config, table):
 
 
 def query_uuid(config, uuid):
-
-    sql = f"""
+    """Get specific uuid from products"""
+    sql = """
         SELECT *
         FROM products
-        where products.id=%s"""
+        where products.id=%s
+        """
 
     with psycopg.connect(**config) as conn:
         with conn.cursor() as curs:
@@ -61,8 +75,9 @@ def query_uuid(config, uuid):
             # It should be one...
             return curs.fetchone()
 
+
 def update_uuid(config, table, uuid, column, value):
-        
+    """Update by uuid"""
     sql = f"""
         UPDATE {table}
         SET {column} = %s
@@ -82,7 +97,9 @@ def update_uuid(config, table, uuid, column, value):
                 return True
             return False
 
+
 def query_all_items(config):
+    """Helper to get all items from products table"""
     sql = """
         SELECT *
         FROM products
