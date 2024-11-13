0e336daafff21c298f4dee351cc9a71a43d4c2db
Thanassis Drivas <thanasisdrivas@gmail.com>
2024-11-14 00:33:55 +0200

    Create utils file, dockerfile and requirements.txt

--- noa-ui/browser/views.py
+++ noa-ui/browser/views.py
@@ -1,24 +1,62 @@
-from django.shortcuts import render
+import requests
+from datetime import datetime
 
 from django.shortcuts import render
 from django.http import JsonResponse
+
+from utils.geo_utils import bbox_to_polygon
+
+API_BASE_URL = "https://localhost:7236/api" 
+
 
 def map_view(request):
     return render(request, 'base.html')
 
 def search(request):
-    if request.method == 'POST':
-        data_source = request.POST.get('data_source')
-        cloud_coverage = request.POST.get('cloud_coverage')
-        start_date = request.POST.get('start_date')
-        end_date = request.POST.get('end_date')
-        geometry = request.POST.get('geometry')
-        # Process the search parameters (e.g., call an external API)
-        response_data = {
-            "data_source": data_source,
-            "cloud_coverage": cloud_coverage,
-            "start_date": start_date,
-            "end_date": end_date,
-            "geometry": geometry,
-        }
-        return JsonResponse(response_data)
+    if request.method == "POST":
+        data_source = request.POST.get("data_source")
+        start_date = request.POST.get("start_date") + "T00:00:00.000"
+        end_date = request.POST.get("end_date", datetime.now().strftime("%Y-%m-%d"))
+        geometry = request.POST.get("bbox")
+        cloud_coverage = request.POST.get("cloud_coverage", None)
+        product_type = request.POST.get("product_type", None)
+        relative_orbit = request.POST.get("relative_orbit", None)
+
+        geometry = [coordinate for coordinate in geometry.split(",")]
+
+        if data_source == "Sentinel-2":
+            payload = {
+                "provider": 2, 
+                "startDate": start_date,
+                "geometry": bbox_to_polygon(geometry),
+                "properties": {
+                    "cloudCoverage": cloud_coverage
+                },
+            }
+            endpoint = f"{API_BASE_URL}/SatelliteProduct"
+        
+        elif data_source == "Sentinel-1":
+            payload = {
+                "provider": 1,  
+                "startDate": start_date,
+                "geometry": bbox_to_polygon(geometry),
+                "properties": {
+                    "productType": product_type,
+                    "relativeOrbit": relative_orbit,
+                },
+            }
+            endpoint = f"{API_BASE_URL}/SatelliteProduct"
+        
+        else:
+            return JsonResponse({"error": "Invalid data source"}, status=400)
+
+        try:
+            response = requests.post(endpoint, json=payload, verify=False) 
+            response.raise_for_status()
+        
+        except requests.exceptions.RequestException as e:
+            return JsonResponse({"error": str(e)}, status=500)
+
+        return JsonResponse(response.json())
+
+    return render(request, "search.html")
