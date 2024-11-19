443fb924718518ce9ca74ecce47ba686d846ba8e
Fotis Balampanis <fbalampanis@protonmail.com>
2024-11-19 16:35:35 +0200

    Introduce Kafka Consumer

--- noa-harvester/noaharvester/messaging/__init__.py
+++ noa-harvester/noaharvester/messaging/__init__.py
@@ -13,3 +13,28 @@ class AbstractProducer(object):
         """
         Send the specified Value to a Kafka Topic.
         """
+
+class AbstractConsumer(object):
+    """
+    Abstract Kafka Consumer doing nothing.
+    """
+    def __init__(self, bootstrap_servers: list, group_id: str, topics: list, schema: dict) -> None:
+        """
+        Create the Consumer instance.
+        """
+        self.bootstrap_servers = bootstrap_servers
+        self.group_id = group_id
+        self.topics = topics
+        self.schema = schema
+
+    def create_topics(self, topics: list, num_partitions: int = 2, replication_factor: int = 1):
+        """
+        Create the specified list of Topics.
+        """
+        return []
+
+    def read(self):
+        """
+        Read messages from the configured Kafka Topics.
+        """
+        return []
