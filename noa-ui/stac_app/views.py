57c5c92309ce10865c8e5e23a03ef5b560158da1
Thanassis Drivas <thanasisdrivas@gmail.com>
2024-11-22 11:52:50 +0200

    Add sidebar with STAC items

--- noa-ui/stac_app/views.py
+++ noa-ui/stac_app/views.py
@@ -1,23 +1,64 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from django.db import connection
+from django.shortcuts import render
 
 @api_view(['GET'])
 def get_collections(request):
+    """
+    Fetches all collections from the pgstac.collections table.
+    """
     with connection.cursor() as cursor:
-        cursor.execute("SELECT id, title, description FROM collections")
+        cursor.execute("""
+            SELECT id, content->>'title' AS title, content->>'description' AS description
+            FROM pgstac.collections
+        """)
         rows = cursor.fetchall()
         collections = [{'id': row[0], 'title': row[1], 'description': row[2]} for row in rows]
     return JsonResponse(collections, safe=False)
 
-@api_view(['GET'])
-def get_items(request, collection_id):
+from django.http import JsonResponse
+from django.db import connection
+import json
+
+def items_page(request, collection_id=None):
+    """
+    Fetch items for a given collection_id and render them in a template.
+    """
+    query = """
+        SELECT id, ST_AsGeoJSON(ST_Transform(geometry, 4326)), content
+        FROM pgstac.items
+    """
+    params = []
+    if collection_id:
+        query += " WHERE collection = %s"
+        params.append(collection_id)
+
     with connection.cursor() as cursor:
-        cursor.execute("""
-            SELECT id, geometry, properties
-            FROM items
-            WHERE collection_id = %s
-        """, [collection_id])
+        cursor.execute(query, params)
         rows = cursor.fetchall()
-        items = [{'id': row[0], 'geometry': row[1], 'properties': row[2]} for row in rows]
-    return JsonResponse(items, safe=False)
+        items = []
+        for row in rows:
+            item_id = row[0]
+            geometry = json.loads(row[1])  # Parse GeoJSON string into a dictionary
+            content = json.loads(row[2])  # Parse content JSON into a dictionary
+            
+            # Merge geometry into the content
+            content['geometry'] = geometry
+            
+            # Append the result
+            items.append({
+                'id': item_id,
+                'geometry': geometry,
+                'properties': content.get('properties', {}),
+                'assets': content.get('assets', {}),
+            })
+
+    # Pass the items to the template
+    return render(request, 'stac_app/index.html', {'items_json': json.dumps(items)})
+
+def index(request):
+    """
+    Renders the index.html template for the STAC application.
+    """
+    return render(request, 'stac_app/index.html')
