8ddea3e2ef88a128198cc71b44ee3102b1f1233d
Thanassis Drivas <thanasisdrivas@gmail.com>
2024-11-22 11:52:50 +0200

    Add sidebar with STAC items

--- noa-ui/stac_app/urls.py
+++ noa-ui/stac_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from stac_app import views
+from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),  # Default view for rendering the UI
     path('collections/', views.get_collections, name='get_collections'),
-    path('collections/<str:collection_id>/items/', views.get_items, name='get_items'),
+    # path('collections/<str:collection_id>/items/', views.get_items, name='get_items'),
+    path('items_page/', views.items_page, name='items_page'),
+
 ]
