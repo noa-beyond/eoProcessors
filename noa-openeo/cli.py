752724778905eeb642c8507ae1b5559b1572fd92
Fotis Balampanis <fbalampanis@protonmail.com>
2025-02-25 23:01:36 +0200

    Proper requirements and cli options

--- noa-openeo/cli.py
+++ noa-openeo/cli.py
@@ -1,10 +1,11 @@
-"""Cli for NOA-Beyond Harvester processor.
+"""Cli for NOA-Beyond CDSE OpenEO operations.
 
-This interface and processor are used to query and download EO data
-from various data providers: Copernicus and Earthdata.
+This interface and processor are used to perform batch operations
+using the openEO python library for CDSE provider and data.
 """
 
 from __future__ import annotations
+import os
 import sys
 import json
 import logging
@@ -15,7 +16,7 @@ from click import Argument, Option
 
 import openeo
 
-from noaopeneo.monthly_median import aggegate_per_tile
+from noaopeneo.monthly_median import month_median
 from noaopeneo import utils
 
 # Appending the module path in order to have a kind of cli "dry execution"
@@ -28,8 +29,7 @@ logger = logging.getLogger(__name__)
     help=(
         "Product generation from Copernicus CDSE using openeo python client "
         "according to parameters as defined in the [CONFIG_FILE]."
-        "It can also receive a [SHAPE_FILE] path as an argument, in order "
-        "for the bounding box to be defined there instead of the [CONFIG_FILE]."
+        "A GeoJson [SHAPE_FILE] path is required as an argument "
     )
 )
 @click.option(
@@ -51,33 +51,19 @@ def cli(log):
     )
 )
 @click.option(
-    "--verbose",
-    "-v",
-    is_flag=True,
-    help="Shows the progress indicator (for Copernicus only)",
-)
-@click.option(
     "--bbox_only",
     "-bb",
     is_flag=True,
     help="Only use multipolygon total bbox, not individual",
 )
 @click.argument("config_file", required=True)
-@click.argument("shape_file", required=False)
+@click.argument("shape_file", required=True)
 @click.option("--output_path", default="./data", help="Output path")
-@click.option(
-    "--tiles",
-    "-t",
-    multiple=True,
-    help="TileId(s). Can be set multiple times"
-)
 def cfmc(
     config_file: Argument | str,
     shape_file: Argument | str,
     output_path: Option | str,
-    tiles: Option | tuple[str],
-    bbox_only: Option | bool,
-    verbose: Option | bool,
+    bbox_only: Option | bool
 ) -> None:
     """
     Produces cloud free monthly composite for given bbox or shape.
@@ -90,17 +76,79 @@ def cfmc(
     logger.debug("Config file: %s", config_file)
     logger.debug("Output path: %s", output_path)
 
-    if shape_file:
-        logger.debug("Shapefile path: %s", shape_file)
-        _shape_file_bbox_list = utils.get_bbox_from_shp(shape_file, bbox_only)
+    logger.debug("Shapefile path: %s", shape_file)
+    shape = shape_file
+    if bbox_only:
+        shape = utils.get_bbox_from_shp(shape_file, bbox_only)
 
     with open(config_file, encoding="utf8") as f:
         _config = json.load(f)
 
     logger.debug("Trying to connect to cdse openeo")
-    connection = openeo.connect("https://openeo.dataspace.copernicus.eu").authenticate_oidc()
-    for tile in tiles:
-        aggegate_per_tile(connection, tile)
+    # client id: env: OPENEO_CLIENT_ID
+    # client secret: env: OPENEO_CLIENT_SECRET
+    client_id = os.getenv("OPENEO_CLIENT_ID", None)
+    client_secret = os.getenv("OPENEO_CLIENT_SECRET", None)
+    connection = openeo.connect(
+        "https://openeo.dataspace.copernicus.eu"
+    ).authenticate_oidc_client_credentials(
+        client_id=client_id,
+        client_secret=client_secret
+    )
+
+    max_cloud_cover = _config["max_cloud_cover"]
+
+    month_median(
+        connection,
+        _config["start_date"],
+        _config["end_date"],
+        shape,
+        max_cloud_cover=max_cloud_cover
+    )
+
+
+@cli.command(
+    help=(
+        "Job info"
+    )
+)
+@click.argument("job_id", required=True)
+def job_info(job_id: Argument | str,):
+    """ Please get the job info """
+    logger.debug("Trying to connect to cdse openeo")
+    # client id: env: OPENEO_CLIENT_ID
+    # client secret: env: OPENEO_CLIENT_SECRET
+    client_id = os.getenv("OPENEO_CLIENT_ID", None)
+    client_secret = os.getenv("OPENEO_CLIENT_SECRET", None)
+    connection = openeo.connect(
+        "https://openeo.dataspace.copernicus.eu"
+    ).authenticate_oidc_client_credentials(
+        client_id=client_id,
+        client_secret=client_secret
+    )
+    print(connection.get(f"/jobs/{job_id}", expected_status=200).json())
+
+
+@cli.command(
+    help=(
+        "Job delete"
+    )
+)
+@click.argument("job_id", required=True)
+def job_delete(job_id: Argument | str,):
+    """ Please get the job info """
+    logger.debug("Trying to connect to cdse openeo")
+    # client id: env: OPENEO_CLIENT_ID
+    # client secret: env: OPENEO_CLIENT_SECRET
+    client_id = os.getenv("OPENEO_CLIENT_ID", None)
+    client_secret = os.getenv("OPENEO_CLIENT_SECRET", None)
+    connection = openeo.connect(
+        "https://openeo.dataspace.copernicus.eu"
+    ).authenticate_oidc_client_credentials(
+        client_id=client_id,
+        client_secret=client_secret
+    )
+    print(connection.delete(f"/jobs/{job_id}", expected_status=204))
 
 
 if __name__ == "__main__":  # pragma: no cover
