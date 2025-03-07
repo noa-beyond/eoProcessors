b049e4972c019bb2c83c94fc42bd2f4a7c25f78b
Fotis Balampanis <fbalampanis@protonmail.com>
2025-03-07 10:39:48 +0200

    Get job assets info

--- noa-openeo/cli.py
+++ noa-openeo/cli.py
@@ -219,5 +219,37 @@ def job_get_assets(job_id: Argument | str,):
         print(f"Downloaded: {local_filename}")
 
 
+@cli.command(
+    help=(
+        "Job job_get_assets"
+    )
+)
+@click.argument("job_id", required=True)
+def job_assets_info(job_id: Argument | str,):
+    """ Get the job assets info (url mainly) """
+    logger.debug("Trying to connect to cdse openeo")
+    # client id: env: OPENEO_CLIENT_ID
+    # client secret: env: OPENEO_CLIENT_SECRET
+    client_id = os.getenv("OPENEO_CLIENT_ID", None)
+    client_secret = os.getenv("OPENEO_CLIENT_SECRET", None)
+    connection = openeo.connect(
+        "https://openeo.dataspace.copernicus.eu"
+    ).authenticate_oidc_client_credentials(
+        client_id=client_id,
+        client_secret=client_secret
+    )
+
+    job = connection.job(job_id)
+
+    # Get result assets (download links)
+    results = job.get_results()
+    assets = results.get_assets()
+
+    print(f"{assets}")
+    print("======= Assets href =======")
+    for asset in assets:
+        print(f"{asset.name}: {asset.href}")
+
+
 if __name__ == "__main__":  # pragma: no cover
     cli()
