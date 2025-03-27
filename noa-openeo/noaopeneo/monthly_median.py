63052de283c66c4680dd14aa707400473ecb60dd
Fotis Balampanis <fbalampanis@protonmail.com>
2025-03-27 13:56:33 +0200

    Black

--- noa-openeo/noaopeneo/monthly_median.py
+++ noa-openeo/noaopeneo/monthly_median.py
@@ -12,8 +12,9 @@ from openeo import Connection
 import logging
 
 log_filename = f"openeo_log_{datetime.now().strftime("%Y-%m-%d-%H:%M:%S")}.log"
-logging.basicConfig(filename=log_filename, level=logging.INFO,
-                    format='%(asctime)s - %(message)s')
+logging.basicConfig(
+    filename=log_filename, level=logging.INFO, format="%(asctime)s - %(message)s"
+)
 
 
 MAX_RETRIES = 5
@@ -21,7 +22,14 @@ SLEEPING_TIME_SEC = 30
 
 
 def monthly_median_daydelta(
-    connection, bands, start_date, end_date, shape, max_cloud_cover, day_delta, output_path
+    connection,
+    bands,
+    start_date,
+    end_date,
+    shape,
+    max_cloud_cover,
+    day_delta,
+    output_path,
 ):
     """Iterates over each month in the range and calls process_dates with expanded range."""
     start_date = datetime.strptime(start_date, "%Y-%m-%d")
@@ -33,7 +41,7 @@ def monthly_median_daydelta(
         month_start = current_date
         month_end = (month_start + relativedelta(months=1)) - timedelta(days=1)
 
-        # Expand range by 10 days before and after
+        # Expand range by "day_delta" days before and after
         start_range = month_start - timedelta(days=day_delta)
         end_range = month_end + timedelta(days=day_delta)
 
@@ -47,7 +55,7 @@ def monthly_median_daydelta(
             end_date=end_range.strftime("%Y-%m-%d"),
             shape=shape,
             max_cloud_cover=max_cloud_cover,
-            output_path=output_path
+            output_path=output_path,
         )
 
         # Move to the next month
@@ -55,7 +63,13 @@ def monthly_median_daydelta(
 
 
 def mask_and_complete(
-    connection: Connection, bands, start_date, end_date, shape, max_cloud_cover, output_path
+    connection: Connection,
+    bands,
+    start_date,
+    end_date,
+    shape,
+    max_cloud_cover,
+    output_path,
 ):
 
     s2_cube = connection.load_collection(
@@ -75,9 +89,7 @@ def mask_and_complete(
     )
 
     # Cloudy pixels are identified as where SCL is 3, 8, 9, or 10.
-    # Here we assume that the client overloads comparison operators and bitwise OR (|) for boolean operations.
-    # [0, 1, 2, 3, 7, 8, 9, 10]
-    # Clouds only [0, 3, 8, 9, 10]
+    # Clouds and 0 as noted from SCL [0, 3, 8, 9, 10]
     cloud_mask = (
         (scl_cube == 0)
         | (scl_cube == 3)
@@ -88,26 +100,28 @@ def mask_and_complete(
     # water_mask = (scl_cube == 6)
     # snow_mask = (scl_cube == 11)
 
-    # Keep only AOI
+    # Mask clouds
     masked_cube_full = s2_cube.mask(cloud_mask, replacement=None)
+    # Keep only AOI
     masked_cube = masked_cube_full.filter_spatial(shape)
 
     for band in bands:
         for retry in range(MAX_RETRIES):
             try:
                 logging.info("%s %s %s - START", band, start_date, end_date)
-                print(f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] Trying band {band} from {start_date} to {end_date}")
+                print(
+                    f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}]"
+                    f"Trying band {band} from {start_date} to {end_date}"
+                )
                 masked_band = masked_cube.band(band)
                 # not_masked_band = masked_cube.band(band)
 
-                # 6. Reduce the time dimension to create a composite.
-                # For example, use a median reducer over time.
                 composite = masked_band.reduce_dimension(
-                    dimension="t", reducer="median"  # or use a custom reducer if needed
+                    dimension="t", reducer="median"
                 )
                 #  not_masked_composite = not_masked_band.reduce_dimension(
                 #     dimension="t",
-                #     reducer="median"  # or use a custom reducer if needed
+                #     reducer="median"
                 # )
 
                 output_dir = str(Path(output_path, "cloud_free_composites"))
@@ -116,34 +130,42 @@ def mask_and_complete(
                 output_file = os.path.join(
                     output_dir, f"{Path(shape).stem}_{start_date}_{end_date}_{band}.tif"
                 )
-                # output_file_not_masked = os.path.join(output_dir, f"{Path(shape).stem}_{start_date}_{end_date}_{band}_not_masked.tif")
+                # output_file_not_masked = os.path.join(
+                #     output_dir,
+                #     f"{Path(shape).stem}_{start_date}_{end_date}_{band}_not_masked.tif"
+                # )
 
                 # TODO job options: tweak CPU/mem etc to check credit usage
                 composite.execute_batch(output_file, out_format="GTiff", title=title)
             except Exception as e:
                 print(
-                    f"Something went wrong: {e}.\n RETRYING({retry} attempt out of {MAX_RETRIES})")
+                    f"Something went wrong: {e}.\n"
+                    f"RETRYING({retry} attempt out of {MAX_RETRIES})"
+                )
                 logging.warning(
                     "Something went wrong: %s. RETRYING(%s attempt out of %s)",
                     e,
                     retry,
-                    MAX_RETRIES
+                    MAX_RETRIES,
                 )
-                time.sleep(SLEEPING_TIME_SEC + (retry*SLEEPING_TIME_SEC*2))
+                time.sleep(SLEEPING_TIME_SEC + (retry * SLEEPING_TIME_SEC * 2))
                 continue
             else:
                 print(f"DONE: {band} {start_date} {end_date} {output_file}")
-                logging.info("DONE: %s %s %s %s", band, start_date, end_date, output_file)
+                logging.info(
+                    "DONE: %s %s %s %s", band, start_date, end_date, output_file
+                )
                 break
         else:
             print(
-                f"Something went wrong after {MAX_RETRIES} attempts. Aborting jobs of {title}")
+                f"Something went wrong after {MAX_RETRIES} attempts. Aborting jobs of {title}"
+            )
             logging.error(
                 "Something went wrong after %s attempts. Aborting jobs of %s",
                 MAX_RETRIES,
-                title
+                title,
             )
-            # TODO continue to next month
+            # Some months (especially early 2017) do not have data. Continue to next month.
             return
             # raise RuntimeError("Something is terribly wrong. Aborting")
 
