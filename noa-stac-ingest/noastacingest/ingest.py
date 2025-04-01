65786e4d09d123ca74246c6582ad6a27b2d987f6
Fotis Balampanis <fbalampanis@protonmail.com>
2025-04-01 15:36:23 +0300

    Refactor to separate Item creation and raise exception if error

--- noa-stac-ingest/noastacingest/ingest.py
+++ noa-stac-ingest/noastacingest/ingest.py
@@ -7,20 +7,14 @@ from pathlib import Path
 import json
 import logging
 
-from stactools.sentinel1.grd.stac import create_item as create_item_s1_grd
-from stactools.sentinel1.rtc.stac import create_item as create_item_s1_rtc
-from stactools.sentinel1.slc.stac import create_item as create_item_s1_slc
-from stactools.sentinel2.commands import create_item as create_item_s2
-from stactools.sentinel3.commands import create_item as create_item_s3
-
 from pystac import Catalog
 
 from noastacingest import utils
 from noastacingest.db import utils as db_utils
+from noastacingest.create_item_copernicus import create_copernicus_item
 from noastacingest.create_item_beyond import create_wrf_item
 
 
-FILETYPES = ("SAFE", "SEN3")
 logger = logging.getLogger(__name__)
 
 
@@ -53,7 +47,7 @@ class Ingest:
         collection: str | None,
         update_db: bool,
         noa_product_id: str | None = None,
-    ):
+    ) -> bool:
         """
         Create a new STAC Item, either by ingestion of existing data or new ones.
         Copernicus products come in directories (with either SAFE or SEN3 extensions)
@@ -61,84 +55,63 @@ class Ingest:
         """
         # Additional provider for the item. Beyond host some Copernicus
         # data but also produces new products.
+        item = {}
         additional_providers = utils.get_additional_providers(collection=collection)
 
-        if path.name.endswith(FILETYPES):
-            platform = str(path.name).split("_", maxsplit=1)[0]
-            satellite = platform[:2]
-            item = {}
-            match satellite:
-                case "S1":
-                    sensor = str(path.name).split("_")[2][:3]
-                    match sensor:
-                        case "GRD":
-                            item = create_item_s1_grd(str(path))
-                            if not collection:
-                                collection = "sentinel1-grd"
-                        case "RTC":
-                            item = create_item_s1_rtc(
-                                granule_href=str(path),
-                                additional_providers=additional_providers,
-                            )
-                            if not collection:
-                                collection = "sentinel1-rtc"
-                        case "SLC":
-                            item = create_item_s1_slc(str(path))
-                            if not collection:
-                                collection = "sentinel1-slc"
-                case "S2":
-                    item = create_item_s2(
-                        granule_href=str(path),
-                        additional_providers=additional_providers,
-                    )
-                    if not collection:
-                        collection = "sentinel2-l2a"
-                case "S3":
-                    item = create_item_s3(str(path))
-                    if not collection:
-                        collection = "sentinel3"
-            item.properties["noa_product_id"] = noa_product_id
-            item_path = (
-                self._config.get("collection_path") + collection + "/items/" + item.id
+        if collection == "wrf":
+            item = create_wrf_item()
+        else:
+            item = create_copernicus_item(
+                path=path,
+                collection=collection,
+                additional_providers=additional_providers
             )
-            # TODO throw error if collection or catalog are not in path
-            json_file_path = str(Path(item_path, item.id + ".json"))
-            print(json_file_path)
+        if not item:
+            logger.error("Could not create STAC Item")
+            return False
+        item.properties["noa_product_id"] = noa_product_id
+        item_path = (
+            self._config.get("collection_path") + collection + "/items/" + item.id
+        )
+        # TODO throw error if collection or catalog are not in path
+        json_file_path = str(Path(item_path, item.id + ".json"))
+        print(json_file_path)
 
-            # TODO?? add to item??:
-            # feature_collection = {
-            #     "type": "FeatureCollection",
-            #     "features": [
-            #          item.to_dict() for item in collection_instance.get_all_items()
-            #     ]
-            # }
+        # TODO?? add to item??:
+        # feature_collection = {
+        #     "type": "FeatureCollection",
+        #     "features": [
+        #          item.to_dict() for item in collection_instance.get_all_items()
+        #     ]
+        # }
 
-            # Catalog and Collections must exist
-            item.set_root(self._catalog)
-            collection_instance = self._catalog.get_child(collection)
-            item.set_collection(collection_instance)
-            # TODO most providers do not have a direct collection/items relation
-            # Rather, they provide an items link, where all items are present
-            # e.g. https://earth-search.aws.element84.com/v1/
-            # collections/sentinel-2-l2a/items
-            # Like so, I do not know if an "extent" property is needed.
-            # If it is, update it:
-            collection_instance.update_extent_from_items()
-            collection_instance.normalize_and_save(
-                self._config.get("collection_path") + collection + "/"
+        # Catalog and Collections must exist
+        item.set_root(self._catalog)
+        collection_instance = self._catalog.get_child(collection)
+        item.set_collection(collection_instance)
+        # TODO most providers do not have a direct collection/items relation
+        # Rather, they provide an items link, where all items are present
+        # e.g. https://earth-search.aws.element84.com/v1/
+        # collections/sentinel-2-l2a/items
+        # Like so, I do not know if an "extent" property is needed.
+        # If it is, update it:
+        collection_instance.update_extent_from_items()
+        collection_instance.normalize_and_save(
+            self._config.get("collection_path") + collection + "/"
+        )
+        if update_db:
+            db_utils.load_stac_items_to_pgstac(
+                [collection_instance.to_dict()], collection=True
             )
-            if update_db:
-                db_utils.load_stac_items_to_pgstac(
-                    [collection_instance.to_dict()], collection=True
-                )
 
-            item.set_self_href(json_file_path)
-            item.save_object(include_self_link=True)
-            if update_db:
-                db_utils.load_stac_items_to_pgstac(
-                    [collection_instance.to_dict()], True
-                )
-                db_utils.load_stac_items_to_pgstac([item.to_dict()])
+        item.set_self_href(json_file_path)
+        item.save_object(include_self_link=True)
+        if update_db:
+            db_utils.load_stac_items_to_pgstac(
+                [collection_instance.to_dict()], True
+            )
+            db_utils.load_stac_items_to_pgstac([item.to_dict()])
+        return True
 
     def from_uuid_db_list(self, uuid_list, collection, db_ingest):
         """Get from products table the paths to ingest"""
@@ -166,12 +139,18 @@ class Ingest:
             item_path = item.get("path")
             # For production the two options should be "None, True"
             try:
-                self.single_item(
+                result = self.single_item(
                     Path(item_path), collection, db_ingest, single_uuid
                 )
-                ingested_items.append(str(single_uuid))
-                logger.debug("Ingested item from %s", item_path)
-            except RuntimeWarning as e:
+                if result:
+                    ingested_items.append(str(single_uuid))
+                    logger.debug("Ingested item from %s", item_path)
+                else:
+                    raise RuntimeError(
+                        "Could not create the STAC Item",
+                        single_uuid
+                    )
+            except RuntimeError as e:
                 logger.error(
                     "Item could not be ingested to pgSTAC: %s", str(single_uuid)
                 )
